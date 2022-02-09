0e7d7a4125928863818a7026d78dae2978ecc93e
Marcos Villarreal <marcos060203@gmail.com>
2022-02-09 16:54:39 -0700

    Folder Changes

---
+++ Homework/Lectures/Variables.py
@@ -0,0 +1,9 @@
+firstname = input("What is your first name?")
+print("hello", firstname)
+
+height= input('How tall are you in inches?')
+print("You are", height, "inches tall")
+
+print("You are", int(height)//12,  
+     "feet", int(height)%12,
+    "inches tall.")
