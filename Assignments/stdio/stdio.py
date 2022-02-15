a8485e7a81b547ce32545962c3a6e5e4937869b9
Marcos Villarreal <marcos060203@gmail.com>
2022-02-15 16:53:33 -0700

    Added Step 2

--- Assignments/stdio/stdio.py
+++ Assignments/stdio/stdio.py
@@ -3,9 +3,12 @@ Name: Marcos Villarreal
 Date: 02/21/22
 
 This program will print various stages or parts of the hangman game
-Step 1: Prompt user to enter their name (# 7)
-Step 2: 
+Step 1: Prompt user to enter their name. Make sure to store name as a function (# 7)
+Step 2: Greet user and restate their name (#8)
 '''
 
 #Step 1:
-input("Please enter your name:")
+name= input("Please enter your name:")
+
+#Step 2: 
+print("Hello,", name)
