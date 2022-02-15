ba757ca6be676ffdcfd79936908209c841ca6755
Marcos Villarreal <marcos060203@gmail.com>
2022-02-15 16:46:08 -0700

    Added Step 1

--- Assignments/stdio/stdio.py
+++ Assignments/stdio/stdio.py
@@ -2,5 +2,10 @@
 Name: Marcos Villarreal
 Date: 02/21/22
 
-This program will 
+This program will print various stages or parts of the hangman game
+Step 1: Prompt user to enter their name (# 7)
+Step 2: 
 '''
+
+#Step 1:
+input("Please enter your name:")
