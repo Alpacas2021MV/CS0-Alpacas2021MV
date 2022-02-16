fa8b0c9f1091c4275aee034c785566667327a5f3
Marcos Villarreal <marcos060203@gmail.com>
2022-02-15 18:53:15 -0700

    Added some text to step 2

--- Assignments/stdio/stdio.py
+++ Assignments/stdio/stdio.py
@@ -4,7 +4,8 @@ Date: 02/21/22
 
 This program will print various stages or parts of the hangman game
 Step 1: Prompt user to enter their name. Make sure to store name as a function (# 7)
-Step 2: Greet user and restate their name (#8)
+Step 2: Greet user and restate their name (#8). Then, state that the game is under construction
+Step 3: Show stage 0, the beginning of Hangman
 '''
 
 #Step 1:
@@ -12,3 +13,7 @@ name= input("Please enter your name:")
 
 #Step 2: 
 print("Hello,", name)
+input("The hangman game is under construction, maybe you’ll get to play it in a few weeks...")
+input("This is what various stages of the hangman game will look like…")
+
+#Step 3: 
