720e90591928e1232549d4da63ff8c84d602c5ff
Marcos Villarreal <marcos060203@gmail.com>
2022-02-19 11:54:04 -0700

    Final Submission

--- Assignments/stdio/stdio.py
+++ Assignments/stdio/stdio.py
@@ -6,7 +6,7 @@ This program will print various stages or parts of the hangman game
 Step 1: Prompt user to enter their name. Make sure to store name as a function (# 7)
 Step 2: Greet user and restate their name (#8). Then, state that the game is under construction
 Step 3: Show stage 0, the beginning of Hangman
-Step 4: Print stages 1 through 7
+Step 4: Print stages 1 through 6
 '''
 
 #Step 1:
@@ -14,8 +14,7 @@ name= input("Please enter your name:")
 
 #Step 2: 
 print("Hello,", name)
-import time 
-print(time.sleep(2))
+
 input("The hangman game is under construction, maybe you’ll get to play it in a few weeks...")
 input("This is what various stages of the hangman game will look like…")
 
@@ -88,5 +87,17 @@ print("|     O")
 print("|    \|/")
 print("|     |")
 print("|    /")
+print("|")
+print("-------------")
+
+#4e
+print("This is the Final Stage")
+
+print("|__________")
+print("|/    |")
+print("|     O")
+print("|    \|/")
+print("|     |")
+print("|    / \\")
 print("|")
 print("-------------")
