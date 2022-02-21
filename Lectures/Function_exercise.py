929f698ccbd4df160fd027a72b5b38900bfde8c1
Marcos Villarreal <marcos060203@gmail.com>
2022-02-21 16:50:58 -0700

    Lecture on Feb 21

--- Lectures/Function_exercise.py
+++ Lectures/Function_exercise.py
@@ -11,17 +11,29 @@ Step 3: Call function with some parameters
 Step 4: Print out answer
 
 '''
-
-
 #Step 1
-from operator import mul 
-
 def multiply_nums(num1, num2):
+    """
+    This function takes in two numbers as parameters 
+    and returns those numbers multiplied together. 
+    """
     #Step 2
-    return num1*num2
 
-#Step 3
-answer = multiply_nums(42, 5)
+    mul_nums = num1*num2
+    return mul_nums
 
-#Step 4
-print("42 * 5 = ", answer)
+def tests():
+    assert multiply_nums(3, 4) == 12
+    assert multiply_nums(7, 8) == 56
+
+def main():
+    tests()
+
+def main(): 
+    number1 = int(input("Please enter a number: "))
+    number2 = int(input("Please enter another number: "))
+    #Step 3
+    answer = multiply_nums(number1, number2)
+    #Step 4
+    print(f"{number1} * {number2} = {answer}")
+main() 
