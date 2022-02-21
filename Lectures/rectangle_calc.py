8e1a99ed8ae221801eddc56085234f608297b0fb
Marcos Villarreal <marcos060203@gmail.com>
2022-02-21 16:51:32 -0700

    Different version of rectangle calc

---
+++ Lectures/rectangle_calc.py
@@ -0,0 +1,20 @@
+'''
+Name: Marcos Villarreal 
+Date: 02/21/22
+
+This program will ask for two sides of a rectangle
+and calculate the area and perimeter
+
+step1: Prompt for two sides
+step2: calc area 
+step calc perimeter 
+step4: print out area and perimeter
+'''
+def main():
+    #Step 1
+    side1 = float(input("Please enter side 1: "))
+    side2= float(input("Please enter side 2: "))
+
+    pass
+
+main()
