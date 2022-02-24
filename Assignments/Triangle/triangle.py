8d89e219f0a6ba313c76827916a9c14064d90742
Marcos Villarreal <marcos060203@gmail.com>
2022-02-23 20:20:35 -0700

    Programmer Info

--- Assignments/Triangle/triangle.py
+++ Assignments/Triangle/triangle.py
@@ -2,5 +2,9 @@
 Name: Marcos Villarreal 
 Date: 02/28/22
 
+This program will ask the user to input three sides of 
+a triangle and will calculate the area and perimeter of the sides. 
+Then, it should print on the screen the area and perimeter 
 
+#Step 1: 
 '''
