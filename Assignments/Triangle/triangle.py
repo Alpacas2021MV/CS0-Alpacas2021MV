e859a1947e5546a130c041a0969119dc0a98aa00
Marcos Villarreal <marcos060203@gmail.com>
2022-02-26 12:24:59 -0700

    Restarted mostly everything and did steps 1 and 2

--- Assignments/Triangle/triangle.py
+++ Assignments/Triangle/triangle.py
@@ -7,34 +7,20 @@ a triangle and will calculate the area and perimeter of the sides.
 Then, it should print on the screen the area and perimeter 
 
 #Step 1: Prompt user to input three numbers (the three sides of the triangle hopefully)
-#Step 2: Calculate the area 
-#Step 3: Calculate the perimeter 
+#Step 2: Calculate the perimeter 
+#Step 2a: Calculate the semi- perimeter
+#Step 3: Calculate the area
 #Step 4: print out area and perimeter 
 #Step 5: Test to see if three sides actually make a triangle 
 '''
 #Step 1 
-side1 = input("Please enter the first side: ")
-side2 = input("Please enter the second side: ")
-side3 = input("Please enter the last side: ")
+a = int(input("Please enter the length of the first side (a): "))
+b = int(input("Please enter the length of the second side (b): "))
+c = int(input("Please enter the length of the third side (c): "))
 
 #Step 2
-def calc_triangle_area(side1, side2, side3):
-    '''
-    This function takes in the three sides inputed 
-    and will calculate the area using Heron's formula
-    '''
-    s = (side1 + side2 + side3)//2
-    return s
+perimeter = a + b + c 
 
 #Step 3 
-def calc_triangle_perim(side1, side2, side3): 
-    '''
-    This function takes in the three sides inputed before 
-    and will calculate the perimeter
-    '''
-    pass
 
-#Step 4
-def main(): 
-
-    pass
+#Step 4 
