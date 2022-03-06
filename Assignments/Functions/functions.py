b1fd8d13c0a88c09d2edb25c987b05bc3dab4c76
Marcos Villarreal <marcos060203@gmail.com>
2022-03-06 00:20:45 -0700

    Added steps

--- Assignments/Functions/functions.py
+++ Assignments/Functions/functions.py
@@ -4,5 +4,15 @@ Date: 03/14/22
 
 This program will perform some arithmetic operations when given two numbers using functions
 
-Step 1
+Step 1: Prompt user to enter two numbers 
+Step 2: Define function that adds the numbers and return sum
+Step 3: Define function that multiplies the two numbers and return product
+Step 4: Define function that divides first number by second number and return quotient
+Step 5: Define function that takes two numbers, subtracts second from first and return difference
+Step 6: Define function that find reminder of first number divided by second number
+Step 7: Define function that find the first to the power of the second number and returns the value
+Step 8: Define a function that takes a number and returns the square-root of the number. 
+Step 9: Call all functions and display answers with descriptions 
+Step 10: Write a test function that to automatically test each function
+Bonus step: Write function that finds the larger of the two numbers
 '''
