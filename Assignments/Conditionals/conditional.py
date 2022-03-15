ec83ce29ef061337abfe56c0b64de3ed9b7d4f2e
Marcos Villarreal <marcos060203@gmail.com>
2022-03-15 17:18:42 -0600

    Added programer info and steps

---
+++ Assignments/Conditionals/conditional.py
@@ -0,0 +1,15 @@
+'''
+Name: Marcos Villarreal
+Date: 03/28/22
+
+This program will compute the values of sum, product, max, min, and average when given five numbers
+
+Step 1: Define function that takes 5 numbers and calculates the sum 
+Step 2: Define function that takes 5 numbers and calculates the product 
+Step 3: Define function that calculates the average
+Step 4: Define function that returns the largest value
+Step 5: Define function that returns the smallest value
+Step 6: Write test function and do test cases for all the previous functions
+Step 6: Prompt user for five numbers
+Step 7: Call functions and display the answers
+'''
