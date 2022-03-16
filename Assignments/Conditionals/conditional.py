73b9756903731512ea9f2de2280b5cbdb9c8bcd6
Marcos Villarreal <marcos060203@gmail.com>
2022-03-16 16:13:57 -0600

    Some changes and started to do calculations

--- Assignments/Conditionals/conditional.py
+++ Assignments/Conditionals/conditional.py
@@ -10,28 +10,46 @@ Step 3: Define function that calculates the average
 Step 4: Define function that returns the largest value
 Step 5: Define function that returns the smallest value
 Step 6: Write test function and do test cases for all the previous functions
-Step 6: Prompt user for five numbers
-Step 7: Call functions and display the answers
+Step 7: Prompt user for five numbers
+Step 8: Call functions and display the answers
 '''
-def sum(): 
-    pass 
+#Step 1 
+def sum_nums(num1, num2, num3, num4, num5):
+    sum = (num1 + num2 + num3 + num4 + num5) 
+    return sum
 
+#Step 2
 def product():
     pass
 
+#Step 3
 def average():
     pass
 
+#Step 4
 def max():
     pass
 
+#Step 5
 def min():
     pass 
 
+#Step 6
 def tests():
     pass
 
-def main(): 
-    pass 
+#Step 7
+def prompt_nums(): 
+    print("Please enter five numbers: ")
+    num1 = float(input("Number 1: "))
+    num2 = float(input("Number 2: "))
+    num3 = float(input("Number 3: "))
+    num4 = float(input("Number 4: "))
+    num5 = float(input("Number 5: "))
+    return num1, num2, num3, num4, num5
+
+#Step 8
+def main():
+    num1, num2, num3, num4, num5 = prompt_nums()
 
 main()
