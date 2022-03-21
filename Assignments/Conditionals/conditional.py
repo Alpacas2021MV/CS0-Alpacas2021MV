aa88a281044cccc14b12e8c8764020e44f115966
Marcos Villarreal <marcos060203@gmail.com>
2022-03-21 13:45:03 -0600

    Finished average step

--- Assignments/Conditionals/conditional.py
+++ Assignments/Conditionals/conditional.py
@@ -12,6 +12,7 @@ Step 5: Define function that returns the smallest value
 Step 6: Write test function and do test cases for all the previous functions
 Step 7: Prompt user for five numbers
 Step 8: Call functions and display the answers
+Bonus Step: Make program run again if user wants to 
 '''
 #Step 1 
 def sum_nums(num1, num2, num3, num4, num5):
@@ -24,22 +25,28 @@ def product(num1, num2, num3, num4, num5):
     return multi
 
 #Step 3
-def average():
-    #aver = sum // 5
-    pass
+def average(num1, num2, num3, num4, num5):
+    return (sum_nums(num1, num2, num3, num4, num5)) / 5
+    
 
 #Step 4
-def max():
+def maxi(num1, num2, num3, num4, num5):
     pass
 
 #Step 5
-def min():
+def mini():
     pass 
 
 #Step 6
 def tests():
-    assert sum_nums(2 + 3 + 5 + 6 + 1) == 17
-    assert sum_nums(1 + 2 + 3 + 4 + 5) == 15
+    assert sum_nums(2, 3, 5, 6, 1) == 17
+    assert sum_nums(1, 2, 3, 4, 5) == 15
+
+    assert product(2, 3, 5, 6, 1) == 180
+    assert product(1, 2, 3, 4, 5) == 120
+
+    assert average(1, 2, 3, 4, 5) == 3
+    assert average(2, 3, 4, 5, 6) == 4
     print("All test cases passed")
 
 #Step 7
@@ -55,7 +62,14 @@ def prompt_nums():
 #Step 8
 def main():
     tests()
-
     num1, num2, num3, num4, num5 = prompt_nums()
+
     print(f"The sum of {num1}, {num2}, {num3}, {num4} and {num5} is {sum_nums(num1, num2, num3, num4, num5)}")
+
+    print(f"The product of {num1}, {num2}, {num3}, {num4} and {num5} is {product(num1, num2, num3, num4, num5)}")
+
+    print(f"The average of {num1}, {num2}, {num3}, {num4} and {num5} is {average(num1, num2, num3, num4, num5)}")
+
+    print(f"The maximum value of {num1}, {num2}, {num3}, {num4} and {num5} is {maxi(num1, num2, num3, num4, num5)}")
+
 main()
