0c63628d46c35d08f0bfb37a974a5106df255ca7
Marcos Villarreal <marcos060203@gmail.com>
2022-03-29 13:10:39 -0600

    Added programer information and description

---
+++ Assignments/Loop/loop.py
@@ -0,0 +1,9 @@
+'''
+Name: Marcos Villarreal 
+Date: 04/04/22
+
+This program will play out a game called "Guess the Number." The program will think of a number, 1 to 20, and the player will have 
+six attempts to guess that number. 
+
+Step 1: 
+'''
