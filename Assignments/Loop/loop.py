a8cb3e9be3a57e726a061e5f26cca01f3bcd11a5
Marcos Villarreal <marcos060203@gmail.com>
2022-04-02 16:06:41 -0600

    Working on step 3

--- Assignments/Loop/loop.py
+++ Assignments/Loop/loop.py
@@ -23,14 +23,31 @@ def prompt_name():
     return name
 
 #Step 2
-def random_number():
+def random_number(name):
+    print(f"Hello {name}, I'm thinking of a number between 1 and 20.")
+    print(f"You got six attempts to guess the number.")
     ran_number = random.randint(1, 20)
+    print(f"the random number is {ran_number}!")
     return ran_number
+
+#Step 3
+def more_guesses(ran_number):
+    keep_guessing = True
+    while(keep_guessing):
+        goagain = input("Take a guess: ")
+        if goagain == ran_number:
+            print(f"You are wrong. Guess again")
+        else:
+            print(f"You are correct")
+    return goagain 
+
+#Step 4 
+def high_or_low(): 
+    pass 
+
 
 def main():
     name = prompt_name()
-    #Step 2
-    print(f"Hello {name}, I'm thinking of a number between 1 and 20.")
-    print(f"You got six attempts to guess the number.Take a guess: ")
-
+    ran_number = random_number(name)
+    more_guesses(ran_number)
 main()
