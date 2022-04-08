5cfae01a10295b30f613a2c3908a4d929e5fdc55
Marcos Villarreal <marcos060203@gmail.com>
2022-04-07 22:14:06 -0600

    Changed the function a bit

--- Assignments/Avion/avion.py
+++ Assignments/Avion/avion.py
@@ -2,10 +2,20 @@
 Name: Marcos Villarreal
 Date: 04/06/22
 
-This program will solve the Kattis problem titled "Avion" 
+This program will solve the Kattis problem titled "Avion" which would point out all CIA blimps 
 '''
+
+def cia_blimps(): 
+    for i in range(5): 
+        blimps = input 
+        if blimps == len("Fbi"):
+            print(i)
+        else:
+            print("HE GOT AWAY!")
+
 def main(): 
-    pass
+    cia_blimps()
+
 
 if __name__ == "__main__":
     main()
