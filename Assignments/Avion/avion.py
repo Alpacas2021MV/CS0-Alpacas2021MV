268acd6e029c3181fb40690f287c62cc2f9cd0c7
Marcos Villarreal <marcos060203@gmail.com>
2022-04-08 17:17:34 -0600

    Some Work

--- Assignments/Avion/avion.py
+++ Assignments/Avion/avion.py
@@ -7,15 +7,15 @@ This program will solve the Kattis problem titled "Avion" which would point out 
 
 def cia_blimps(): 
     for i in range(5): 
-        blimps = input 
-        if blimps == len("Fbi"):
-            print(i)
-        else:
-            print("HE GOT AWAY!")
+        blimps = input()
+        if 'FBI' in blimps:
+            print(blimps)
+    else: 
+        print("HE GOT AWAY!")
+         
 
 def main(): 
     cia_blimps()
-
 
 if __name__ == "__main__":
     main()
