9687827cba24167d6cbdec8c674e16cc8e11c22b
Marcos Villarreal <marcos060203@gmail.com>
2022-04-22 16:31:20 -0600

    Made file

---
+++ Assignments/Falling Apart/fallingapart.py
@@ -0,0 +1,17 @@
+from sys import *
+
+def test():
+    assert 
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    if (len(argv) == 2 and argv [1] == "test"):
+        test()
+    else:
+        main()
