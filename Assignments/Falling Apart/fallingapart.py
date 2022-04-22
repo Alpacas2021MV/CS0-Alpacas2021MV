84d2a33988971f29db73463d5a901f63565ce55f
Marcos Villarreal <marcos060203@gmail.com>
2022-04-22 17:56:57 -0600

    Worked on my_input function

--- Assignments/Falling Apart/fallingapart.py
+++ Assignments/Falling Apart/fallingapart.py
@@ -1,7 +1,16 @@
 from sys import *
 
 def test():
-    assert 
+    pass
+
+def my_input(): 
+    my_string = input()
+    my_nums = []
+
+    for i in range(len(my_string.split)):
+        my_nums.append(int(my_string.split[i]))
+        my_nums.sort(reverse = True)
+        
 
 
 def main():
