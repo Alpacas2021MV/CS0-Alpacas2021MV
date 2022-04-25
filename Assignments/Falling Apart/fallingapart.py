127a99d635565724fa2d6ec1a9408780e6d45d26
Marcos Villarreal <marcos060203@gmail.com>
2022-04-25 11:27:27 -0600

    Final submission

--- Assignments/Falling Apart/fallingapart.py
+++ Assignments/Falling Apart/fallingapart.py
@@ -1,36 +1,40 @@
 from sys import *
 
 def test():
-    pass
+    assert my_output([3, 2, 2]) == (5 ,2)
+    assert my_output([7, 5, 2, 1]) == (9 ,6)
+    assert my_output([9, 5, 3, 2, 1]) == (13 ,7)
+    print("All testcases passed")
 
 def my_input(): 
+    tmp = input()
     my_string = input()
     my_nums = []
+    my_string = my_string.split()
+    for i in range(len(my_string)):
+        my_nums.append(int(my_string[i]))
+    my_nums.sort(reverse = True)
+        #Should now be [2, 2, 1, 1] for example (Greatest to Smallest)
+    return my_nums
 
-    for i in range(len(my_string.split)):
-        my_nums.append(int(my_string.split[i]))
-        my_nums.sort(reverse = True)
-        #Should now be [2, 2, 1, 1] (Greatest to Smallest)
-    return my_nums, i
-
-def my_output(my_nums, i):
-    A = 0
-    B = 0
-    for A in my_nums:
-        my_nums[i]
-    for B in my_nums:
-        my_nums[i+1]
-
-    print(A)
-    print(B)
+def my_output(my_nums):
+    alice = 0
+    bob = 0
+    alice_turn = True
+    #https://github.com/KentGrigo/Kattis/blob/master/python/fallingapart.py Got the code below from here. 
+    for nums in my_nums: 
+        if alice_turn:
+            alice += nums
+        else:
+            bob += nums
+        alice_turn = not alice_turn
+    return alice, bob
 
 def main():
-    my_input()
-    my_output()
-
-
-
-
+    test()
+    my_nums = my_input()
+    alice, bob = my_output(my_nums)
+    print(alice, bob)
 
 if __name__ == "__main__":
     if (len(argv) == 2 and argv [1] == "test"):
