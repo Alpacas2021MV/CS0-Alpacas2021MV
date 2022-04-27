4079939857bb7246c0cbeb6be832146f7d26fdd1
Marcos Villarreal <marcos060203@gmail.com>
2022-04-27 16:08:27 -0600

    Created file and skeleton code

---
+++ Assignments/Final/final.py
@@ -0,0 +1,23 @@
+'''
+Name: Marcos Villarreal
+Date: 05/17/22
+
+This program will play out the game of Hangman
+
+Step 1:
+'''
+import sys
+
+def tests(): 
+    pass 
+
+def main(): 
+    pass
+
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 2 and sys.argv[1] == "test"):
+        tests()
+    else:
+        main()
